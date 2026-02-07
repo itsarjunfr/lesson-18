30cf8655ed3f517d97673c600067dd3bd172a6db
itsarjunfr <itsarjunfr@gmail.com>
2026-02-07 17:32:28 +0800

    initial commit

---
+++ valid.py
@@ -0,0 +1,11 @@
+valid = False
+
+while not valid:
+    try:
+        number = int(input('Enter a number: '))
+        while number%2==0:
+            print('bye')
+            valid = True
+            
+    except ValueError: 
+        print('Invalid.')
